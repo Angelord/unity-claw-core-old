4d7774212b84d5873cef3af2036fa63df35f89ba
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-06-14 20:24:38 +0300

    Implement Obstacle Avoidance

---
+++ Assets/Claw/AI/Steering/ObstacleAvoidance.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace Claw.AI.Steering {
+    public class ObstacleAvoidance : SteeringBehaviour {
+
+        [SerializeField] private string obstacleLayer = "Obstacles";
+        [SerializeField] private float seeingDist = 10.0f;
+        [SerializeField] private float avoidanceWidth = 4.0f;
+        private RaycastHit2D hitInfo;
+        private bool hit;
+        
+        protected override Vector2 DoForceCalculation() {
+            if (hitInfo) {
+                //The closer the obstacle is, the greater the steering force should be
+                float multiplier = 1.0f + hitInfo.distance / seeingDist;
+
+                Vector2 steeringDir = hitInfo.point - (Vector2)hitInfo.transform.position;
+                steeringDir.Normalize();
+                steeringDir *= multiplier;
+                return steeringDir;
+                
+                //TODO : Implement a tweakable breaking force
+            }
+         
+            return Vector2.zero;
+        }
+
+        private void FixedUpdate() {
+            Vector2 boxSz = new Vector2(avoidanceWidth / 2.0f, avoidanceWidth / 2.0f);
+            int layerMask = LayerMask.GetMask(obstacleLayer);
+            hitInfo = Physics2D.BoxCast(transform.position, boxSz, 0.0f, transform.up, seeingDist, layerMask);
+        }
+        
+        private void OnDrawGizmosSelected() {
+            Matrix4x4 rotationMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+            Gizmos.matrix = rotationMatrix;
+            Gizmos.color = hitInfo ? Color.red : Color.yellow;
+            Vector3 cubeSize = new Vector3(avoidanceWidth, seeingDist, 1.0f);
+            Gizmos.DrawWireCube (Vector2.up * seeingDist / 2, cubeSize);
+        }
+    }
+}
