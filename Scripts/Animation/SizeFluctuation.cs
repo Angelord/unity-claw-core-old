9cec18d52f8ce31d3014396459e1d32a227ae3a0
Angelord <Georgiev1997@gmail.com>
2021-01-20 00:44:54 +0200

    Add SizeFluctuation component.

---
+++ Scripts/Animation/SizeFluctuation.cs
@@ -0,0 +1,21 @@
+﻿﻿using UnityEngine;
+
+
+namespace Claw.Animation {
+    public class SizeFluctuation : MonoBehaviour {
+
+        [SerializeField][Range(0.0f, 0.1f)] private float maxFluctuation = 0.03f;
+        [SerializeField] private float fluctuationRate = 0.1f;
+        private Vector2 initialScale;
+        
+        private void Start() {
+            initialScale = transform.localScale;
+            InvokeRepeating(nameof(ModifyScale), 0.0f, fluctuationRate);
+        }
+
+        private void ModifyScale() {
+
+            transform.localScale = initialScale * Random.Range(1.0f - maxFluctuation, 1.0f + maxFluctuation);
+        }
+    }
+}
