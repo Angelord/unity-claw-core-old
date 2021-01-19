2a784b0e68b78c6678598cdaeaccff80bf40c6ed
Angelord <Georgiev1997@gmail.com>
2021-01-20 00:14:36 +0200

    Add ScreenShader class.

---
+++ Scripts/Rendering/ScreenShader.cs
@@ -0,0 +1,17 @@
+﻿﻿using UnityEngine;
+
+namespace Claw.Rendering {
+    /// <summary>
+    /// Applies a post-processing shader prior to displaying the rendered scene.
+    /// </summary>
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(UnityEngine.Camera))]
+    public class ScreenShader : MonoBehaviour {
+
+        [SerializeField] private Material effectMaterial = default;
+        
+        private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+            Graphics.Blit(src, dest, effectMaterial);
+        }
+    }
+}
