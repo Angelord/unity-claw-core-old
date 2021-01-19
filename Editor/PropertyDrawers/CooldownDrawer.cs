8eef53dd675589234c9a75360f8337154f499aae
Angelord <Georgiev1997@gmail.com>
2021-01-20 00:32:26 +0200

    Add Cooldown utility class, along with a custom property drawer.

---
+++ Editor/PropertyDrawers/CooldownDrawer.cs
@@ -0,0 +1,31 @@
+﻿using Claw.Chrono;
+using UnityEditor;
+using UnityEngine;
+
+namespace Claw.Editors.PropertyDrawers {
+    
+    [CustomPropertyDrawer(typeof(Cooldown))]
+    public class CooldownDrawer : PropertyDrawer {
+        // Draw the property inside the given rect
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            // Using BeginProperty / EndProperty on the parent property means that
+            // prefab override logic works on the entire property.
+
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Draw label
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            
+            // Don't make child fields be indented
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("duration"), GUIContent.none);
+            
+            // Set indent back to what it was
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Scripts/Chrono/Cooldown.cs
@@ -0,0 +1,28 @@
+﻿﻿using UnityEngine;
+
+namespace Claw.Chrono {
+    [System.Serializable]
+    public class Cooldown {
+
+        [SerializeField] private float duration;
+
+        private float lastReset;
+
+        public bool Ready => TimeSinceLastReset > duration;
+
+        public float Duration { get => duration; set => duration = value; }
+
+        public float TimeRemaining => duration - TimeSinceLastReset;
+
+        private float TimeSinceLastReset => Time.time - lastReset;
+
+        public Cooldown(float duration) {
+            this.duration = duration;
+            lastReset = Time.time;
+        }
+
+        public void Reset() {
+            lastReset = Time.time;
+        }
+    }
+}
