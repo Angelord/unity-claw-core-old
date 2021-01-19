68db44a38f6739739cd9550c7d1a21f6a8f58c91
Angelord <Georgiev1997@gmail.com>
2021-01-20 00:23:02 +0200

    Add Float Range utility type, along with custom property drawer.

---
+++ Editor/PropertyDrawers/FloatRangeDrawer.cs
@@ -0,0 +1,38 @@
+﻿using Claw.Utility;
+using UnityEditor;
+using UnityEngine;
+
+namespace Claw.Editors.PropertyDrawers {
+    [CustomPropertyDrawer(typeof(FloatRange))]
+    public class FloatRangeDrawer : PropertyDrawer {
+        // Draw the property inside the given rect
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            // Using BeginProperty / EndProperty on the parent property means that
+            // prefab override logic works on the entire property.
+
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Draw label
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            // Don't make child fields be indented
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            position.width = 50;
+
+            EditorGUI.LabelField(position, "min");
+            position.x += 30;
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("min"), GUIContent.none);
+            position.x += 70;
+            EditorGUI.LabelField(position, "max");
+            position.x += 30;
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("max"), GUIContent.none);
+
+            // Set indent back to what it was
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Scripts/Utility/FloatRange.cs
@@ -0,0 +1,35 @@
+﻿﻿using System;
+using UnityEngine;
+
+namespace Claw.Utility {
+    [Serializable]
+    public class FloatRange {
+
+        [SerializeField] private float min;
+        [SerializeField] private float max;
+
+        public float Min { get => min; set => min = value; }
+
+        public float Max { get => max; set => max = value; }
+
+        public FloatRange() { }
+
+        public FloatRange(float min, float max) {
+            Set(min, max);
+        }
+
+        public float Random() {
+            return UnityEngine.Random.Range(min, max);
+        }
+
+        public float Lerp(float t) {
+            return Mathf.Lerp(min, max, t);
+        }
+
+        public void Set(float min, float max) {
+            UnityEngine.Assertions.Assert.IsTrue(min <= max, "Invalid range provided for float Range");
+            this.min = min;
+            this.max = max;
+        }
+    }
+}
