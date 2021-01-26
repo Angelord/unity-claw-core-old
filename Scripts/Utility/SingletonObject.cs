b75f86427ee825bec289a18b7b327ca1747187f4
Angelord <Georgiev1997@gmail.com>
2021-01-27 00:08:05 +0200

    Add the SingletonObject utility class.

---
+++ Scripts/Utility/SingletonObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Claw.Utility {
+    public abstract class SingletonObject<T> : MonoBehaviour where T : MonoBehaviour {
+
+        private static T instance;
+
+        public static T Instance {
+            get {
+                if (instance == null) {
+                    instance = (new GameObject(typeof(T).Name)).AddComponent<T>();
+                }
+
+                return instance;
+            }
+        }
+
+        private void Start() {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
