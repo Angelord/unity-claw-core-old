52b7c5f707b33066fdb47cbd04a0258541a885fd
Angelord <Georgiev1997@gmail.com>
2021-06-12 19:31:46 +0300

    Add two new animation behaviours.

---
+++ Scripts/Animation/BobAnim.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Claw.Animation {
+    public class BobAnim : MonoBehaviour {
+
+        [SerializeField] private float range = 1.0f;
+
+        [SerializeField] private float speed = 1.0f;
+
+        private Vector2 initialPos;
+        
+        private void OnEnable() {
+            initialPos = transform.position;
+        }
+
+        private void Update() {
+            transform.position = initialPos + range * Mathf.Sin(Time.time * speed) * Vector2.up;
+        }
+    }
+}
---
+++ Scripts/Animation/SizeSineAnim.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Claw.Animation {
+    public class SizeSineAnim : MonoBehaviour {
+        
+        [SerializeField][Range(0.0f, 1.0f)] private float maxFluctuation = 0.03f;
+        
+        [SerializeField] private float speed = 0.1f;
+        
+        private Vector2 initialScale;
+
+        private void Start() {
+            initialScale = transform.localScale;
+        }
+
+        private void Update() {
+            transform.localScale = initialScale + Mathf.Sin(Time.time * speed) * maxFluctuation * Vector2.one;
+        }
+    }
+}
