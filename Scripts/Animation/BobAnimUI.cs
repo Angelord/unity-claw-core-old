6474ac72fa0dc555fba3fb5684ac2984968dbdb3
Angelord <Georgiev1997@gmail.com>
2021-06-13 11:58:14 +0300

    Add ui verson of BobAnim.

---
+++ Scripts/Animation/BobAnimUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Claw.Animation {
+    public class BobAnimUI : MonoBehaviour {
+        
+        [SerializeField] private float range = 1.0f;
+
+        [SerializeField] private float speed = 1.0f;
+
+        private RectTransform rectTransform;
+
+        private Vector2 initialPos;
+
+        private void Awake() {
+            rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void OnEnable() {
+            initialPos = rectTransform.anchoredPosition;
+        }
+
+        private void Update() {
+            
+            rectTransform.anchoredPosition = initialPos + range * Mathf.Sin(Time.time * speed) * Vector2.up;
+        }
+    }
+}
