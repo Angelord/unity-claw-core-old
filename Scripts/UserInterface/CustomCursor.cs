cc0714a3723528d2268a6e97bf391e2128c3034d
Angelord <Georgiev1997@gmail.com>
2021-06-19 15:51:25 +0300

    Add the custom cursor class to claw.

---
+++ Scripts/UserInterface/CustomCursor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Claw.UserInterface {
+    [CreateAssetMenu(fileName = "Cursor", menuName = "Tentacles/Cursor", order = 1)]
+    public class CustomCursor : ScriptableObject {
+        
+        public Texture2D cursorTexture;
+        
+        public CursorMode cursorMode = CursorMode.Auto;
+        
+        public Vector2 hotSpot = Vector2.zero;
+
+        public void Set() {
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        }
+    }
+}
